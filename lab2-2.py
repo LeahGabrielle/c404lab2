7a0ef415bb051dd420bfa20157c124eea84c98e8
LeahGabrielle <olexson@ualberta.ca>
2016-09-14 15:02:59 -0600

    p4

--- lab2-2.py
+++ lab2-2.py
@@ -1,11 +1,9 @@
-Content-Type: text/enriched
-Text-Width: 70
-
 import socket
 
 #Allocate a new socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+
 #Listen on port 8000
 server.bind(('0.0.0.0', 8000))
 server.listen(1)
@@ -18,14 +16,15 @@ print address
 #client is going to be curl, web browser or something like that
 outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 outgoing.connect(("www.google.com", 80))
+outgoing.setblocking(0)
+client.setblocking(0)
 while True:
     part = client.recv(1024)
-    print "<< " + part
+    print "< " + part
     if (part):
        outgoing.sendall(part)
     part = outgoing.recv(1024)
     print ">" + part
     if (part):
-    client.sendall(part)
-
+       client.sendall(part)
     
